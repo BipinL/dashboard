03fc0c9fc90fe7c76da56559b8176cf45b723e3b
BipinL <104549520+BipinL@users.noreply.github.com>
2022-06-22 19:30:15 +0545

    general setting created

---
+++ resources/views/backend/company/index.blade.php
@@ -0,0 +1,18 @@
+@extends('backend.app')
+@section('content')
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <span>General Setting</span>
+                        </div>
+                        <div class="card-body"></div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Backend\CompanyController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('company', CompanyController::class);
